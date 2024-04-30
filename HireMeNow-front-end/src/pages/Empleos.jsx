adee21770836e990fe43163a70fb6ca7ab73ff82
Santiago Gutierrez Hincapie <gutierrez.santiago@correounivalle.edu.co>
2024-04-30 11:19:56 -0500

    First commit, feature Empleo page

--- HireMeNow-front-end/src/pages/Empleos.jsx
+++ HireMeNow-front-end/src/pages/Empleos.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import './styles/Empleos.css'
 
 function Empleos() {
     return (
-        <div>
-            <h1>Empleos</h1>
+        <div className='main-box'>
+            <div className='container'>
+                <div className='filter'>
+                    <div className='filter-box'>
+
+                    </div>
+                </div>
+                <div className='jobs'>
+                    <div className='filter-box'>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

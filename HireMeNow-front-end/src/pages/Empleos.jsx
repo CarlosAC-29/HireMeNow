0bf4595ef0212af41ac63274d9903f171b8ee76f
Santiago Gutierrez Hincapie <gutierrez.santiago@correounivalle.edu.co>
2024-05-01 22:50:30 -0500

    Jobs list done

--- HireMeNow-front-end/src/pages/Empleos.jsx
+++ HireMeNow-front-end/src/pages/Empleos.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
 import './styles/Empleos.css'
+import {
+    Box,
+    Stack,
+    Typography,
+    TextField,
+    Grid,
+    Button,
+    FormControl,
+    MenuItem,
+    InputLabel,
+    Select,
+    OutlinedInput,
+    Chip,
+    createTheme
+} from '@mui/material';
 
 function Empleos() {
     return (
         <div className='main-box'>
-            <div className='container'>
-                <div className='filter'>
-                    <div className='filter-box'>
+            <Stack sx={{ width: "100%", alignItems: "center" }}>
+                <Box sx={{ background: "#FFFFFF", width: "50%", borderRadius: "0.8rem" }}>
+                    <Stack direction="column" spacing={2} sx={{ padding: "1rem" }}>
+                        <Typography sx={{ fontSize: "1rem", color: "#3673AA" }}>Titulo de empleo</Typography>
+                        <Typography sx={{ fontSize: "0.8rem" }}>Multinacional española especializada en soluciones Smart City ( Portales, dispositivos hardware en vía pública, comunicaciones, IA, etc...) está en proceso de selección de programadores Angular Full Stack. Los profesionales seleccionados se incorporarán a un equipo internacional...</Typography>
+                        <Button variant="contained" sx={{ background: "#3673AA", width: "10%", height: "2rem" }}>Visitar</Button>
 
-                    </div>
-                </div>
-                <div className='jobs'>
-                    <div className='filter-box'>
-
-                    </div>
-                </div>
-            </div>
+                    </Stack>
+                </Box>
+            </Stack>
         </div>
     )
 }
